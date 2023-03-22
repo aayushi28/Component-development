04b63a7f2c7f2cd4726b9bbcc06b26c43a39e148
aayushi28 <aayushi.mandlik@gmail.com>
2023-03-22 02:55:13 -0400

    creating reusable hook

--- src/Component/SortableTable.js
+++ src/Component/SortableTable.js
@@ -1,21 +1,59 @@
 import Table from "./Table";
-function SortableTable(props){
-    const { config } = props;
-    const handleClick = (label) => {
-        console.log(label);
+import { GoArrowSmallDown, GoArrowSmallUp } from "react-icons/go";
+import useSort from "../Hooks/use-sort";
+function SortableTable(props) {
+  const { config, data } = props;
+  const { sortOrder, sortBy, sortedData, setSortColumn } = useSort(data, config);
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
     }
-    const updatedConfig = config.map((column)=>{
-        if(!column.sortValue){
-            return column;
-        }
-        return {
-            ...column,
-            header: () => <th onClick={()=>handleClick(column.label)}>{column.label} IS SORTABLE</th>
-        }
-    })
-    return(
-        <Table {...props} config={updatedConfig}/>
-    );
-}
+    return {
+      ...column,
+      header: () => (
+        <th
+          className="cursor-pointer hover:bg-gray-100"
+          onClick={() => setSortColumn(column.label)}
+        >
+          <div className="flex items-center">
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
+        </th>
+      ),
+    };
+  });
 
+  return <Table {...props} data={sortedData} config={updatedConfig} />;
+}
+function getIcons(label, sortBy, sortOrder) {
+  if (label !== sortBy) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
+  }
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    );
+  } else if (sortOrder === "asc") {
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    );
+  } else if (sortOrder === "desc") {
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    );
+  }
+}
 export default SortableTable;
